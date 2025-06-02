63597932bf061a4b29d69d3ec9fdcce76447d3aa
Sofiia Zinchenko <h12227338@zinchenko.wu.ac.at.>
2025-06-02 19:12:37 +0000

    looks sus

--- evaluation/location_wise_eval_smoothing_2.py
+++ evaluation/location_wise_eval_smoothing_2.py
@@ -1,4 +1,4 @@
-from location_wise_eval_smoothing import *
+from evaluation.location_wise_eval_smoothing import *
 
 distances2 = [5000,10000]
 run_smoothing_eval(distances2, graph_filepath, coordinates, alpha, smoothing_param, smoothing_dir)
