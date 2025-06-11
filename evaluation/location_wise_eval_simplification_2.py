f53ae8e4cd5962f3e46a14cbb514b4dbf6e89821
Selbonyka <sonyazinchenko.me@gmail.com>
2025-06-11 18:22:55 +0200

    oop

--- evaluation/location_wise_eval_simplification_2.py
+++ evaluation/location_wise_eval_simplification_2.py
@@ -1,4 +1,4 @@
 from evaluation.location_wise_eval_simplification import *
 
 distances2 = [5000, 10000]
-run_simplification_eval(distances, graph_filepath, coordinates, alpha, simplification_list, simplifications_dir)
+run_simplification_eval(distances2, graph_filepath, coordinates, alpha, simplification_list, simplifications_dir)
