64386be5e557499abb0158024c5beafc750ccd23
Selbonyka <sonyazinchenko.me@gmail.com>
2025-06-11 18:19:25 +0200

    simplification_eval

---
+++ evaluation/location_wise_eval_simplification.py
@@ -0,0 +1,23 @@
+import os
+
+from evaluation.eval_utils.eval_utils import generate_random_coordinates
+from evaluation.eval_utils.overall_eval_functions import run_overall_evaluation, load_graph
+
+
+def run_simplification_eval(distances, graph_filepath, coordinates_list, alpha, simplification_list, dir):
+    for s in simplification_list:
+        current_dir = os.path.join(dir, str(s))#multiplying by 100 just to make it more computer friendy
+        run_overall_evaluation(distances, graph_filepath, coordinates_list, alpha, s, current_dir)
+
+
+graph_filepath = "/home/h12227338/LoopBuddy/preloadedmap/Wien.pkl"
+
+simplification_list = range(0,150,10)
+distances = [2000,15000]
+alpha = 0.85
+simplifications_dir = "simplification_results"
+coordinates = generate_random_coordinates(20,11111,load_graph(graph_filepath))
+smoothing_param = 0
+
+if __name__ == "__main__": # so it doesnt run on import
+    run_simplification_eval(distances, graph_filepath, coordinates, alpha, simplification_list, simplifications_dir)
---
+++ evaluation/location_wise_eval_simplification_2.py
@@ -0,0 +1,4 @@
+from evaluation.location_wise_eval_simplification import *
+
+distances2 = [5000, 10000]
+run_simplification_eval(distances, graph_filepath, coordinates, alpha, simplification_list, simplifications_dir)
