4b4bad08d02621f9e91a4dd8e44ad8b992f96bf3
Selbonyka <sonyazinchenko.me@gmail.com>
2025-06-11 13:29:02 +0200

    oops

--- evaluation/location_wise_eval_smoothing_2.py
+++ evaluation/location_wise_eval_smoothing_2.py
@@ -1,4 +1,4 @@
 from evaluation.location_wise_eval_smoothing import *
-alpha = 85
+alpha = [85]
 distances2 = [5000,10000]
 run_smoothing_eval(distances2, graph_filepath, coordinates, alpha, smoothing_param, smoothing_dir)
